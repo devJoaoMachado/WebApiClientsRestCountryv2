0313d32abf9fab0417c2fb937cd0a72fd3af74f9
devJoaoMachado <57191181+devJoaoMachado@users.noreply.github.com>
2021-06-18 01:34:59 -0300

    Add files via upload

---
+++ WebApiWithClientFactory/Controllers/CountryClient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiWithClientFactory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryClient : Controller
+    {
+        private HttpClient _httpClient;
+
+        public CountryClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetCountryInformation(string name)
+        {
+            var result = await _httpClient.GetAsync($"https://restcountries.eu/rest/v2/name/{name}");
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("apiinfo")]
+        public  IActionResult GetApiInformation()
+        {
+            var info = new
+            {
+                Memory = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024 + "MB"
+            };
+
+            return Ok(info);
+        }
+    }
+}
---
+++ WebApiWithoutClientFactory/Controllers/CountryClient.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiWithClientFactory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryClient : Controller
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetCountryInformation(string name)
+        {
+            HttpClient httpClient = new HttpClient();
+            var result = await httpClient.GetAsync($"https://restcountries.eu/rest/v2/name/{name}");
+
+            return result;
+        }
+
+        [HttpGet]
+        [Route("apiinfo")]
+        public IActionResult GetApiInformation()
+        {
+            var info = new
+            {
+                Memory = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024 + "MB"
+            };
+
+            return Ok(info);
+        }
+    }
+}
